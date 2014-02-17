2f5fb49aac90eb92c75972f2b8eb9e014247d0dc
greenfairy <katarzyna.terek@gmail.com>
2014-02-17 17:22:02 +0000

    current state

--- src/gradientDomainCompression/__init__.py
+++ src/gradientDomainCompression/__init__.py
@@ -42,16 +42,17 @@ class fattal(hdr.HDR):
             self.fBeta = fBeta
     
     def solvePoisson(self, function):
-        
         fshape = function.shape
         n = fshape[0]*fshape[1]
-        '''b vector'''
-        b=np.reshape(function, n,1)
         
-        '''Build a matrix'''
-        fours = np.ones(1,n)
+        ##figure out reshape
+        b = - np.reshape[function, n, 1]
+        
+        #Build a matrix
+        fours = np.ones((1,n))
         for i in range(0,n):
             fours(i)=4*fours(i)
+        
         A = np.spdiags(fours, 0, n, n)
         T = np.ones((n,1))
         O = T
@@ -65,24 +66,6 @@ class fattal(hdr.HDR):
         #get Bvert
         A = A + B + Bvert
         
-        
-        
-        '''
-        %b vector
-b = -reshape(f,r*c,1);
-
-%Build A matrix
-A = spdiags((4+smoothingCost)*ones(n,1),0,n,n);
-T = ones(n,1);
-O = T;
-T(1:r:n) = 0;
- 
-A = A + B + B';
-
-%Solve Poisson equation: Ax = b
-x = A\b;
-x = x(1:n);
-x = reshape(x, r, c);'''
         #Solve Poisson equation: Ax = b
         x =A/b
         cutoff = np.zeros((1,n))
@@ -91,29 +74,11 @@ x = reshape(x, r, c);'''
             
         #figure out reshape!!!
         x = reshape [cutoff, self.width, self.height]
-        return x    
+        return x
         
     def gaussianPyramid(self, logLuminance):
-        '''Computing Gaussian Pyramid + Gradient'''
-        '''%Computing Gaussian Pyramid + Gradient
-
-
-kernel = [1,4,6,4,1]'*[1,4,6,4,1];
-kernel = kernel/sum(kernel(:));
-
-%Generation of the pyramid
-G = [[], struct('fx',imfilter(L,kernelX,'same')/2,'fy',imfilter(L,kernelY,'same')/2)];
-G2 = sqrt(G(1).fx.^2+G(1).fy.^2);
-fAlpha = 0.1*mean(G2(:));
-
-imgTmp = L;
-for i=1:numPyr
-    imgTmp=imresize(conv2(imgTmp,kernel,'same'),0.5,'bilinear');
-    Fx = imfilter(imgTmp,kernelX,'same')/(2^(i+1));
-    Fy = imfilter(imgTmp,kernelY,'same')/(2^(i+1));
-    G = [G, struct('fx',Fx/(2^(i+1)),'fy',Fy/(2^(i+1)))];
-end'''
-        numPyr  = round(np.log2(min([self.width,self.length])))-np.log2(32)
+        #Computing Gaussian Pyramid + Gradient
+        numPyr = round(np.log2(min([self.width,self.length])))-np.log2(32)
         
         Xkernel = [[0, 0, 0],
                    [-1, 0, 1],
@@ -123,83 +88,90 @@ end'''
                    [0,0,0],
                    [0,-1,0]]
         
-        xs = [1,4,6,4,1]
-        ys = [1,4,6,4,1]
-        vx, vy = np.meshgrid(xs, ys)
-        kernel = vx*vy
-        kernel = kernel/np.sum(kernel)
+        Xfiltered = np.correlate(logLuminance, Xkernel)
+        Yfiltered = np.correlate(logLuminance, Ykernel)
         
-        '''Generation of the pyramid'''
-
-        fx = np.correlate(logLuminance, Xkernel, "same")
-        fy = np.correlate(logLuminance, Ykernel, "same")
-        
-        flat = np.zeros(1, self.width*self.height)
-        i = 0
-        
-        for x in range (0, self.width - 1):
-            for y in range(0, self.height - 1):
-                pyramidGradient[x][y] = (Xfiltered[x]/2,Yfiltered[y]/2)
-                #NOT ENTIRELY SURE ABOUT THE SECOND PART IF IT'S CORRECT INDEX
-                #should maybe be [0]?
-                pG2[x][y] = np.sqrt(pyramidGradient[x][y][0]^2+pyramidGradient[x][y][1]^2)
-                flat[i] = pG2[x][y]
-                i++
-                
-        fAlpha = 0.1*np.mean(pG2)
-            
-        '''Generation of the pyramid'''
+                '''Generation of the pyramid'''
         kx=np.matrix([1,4,6,4,1])
         ky=np.matrix([[1],[4],[6],[4],[1]])
-        kernel = np.zeros(shape=(self.width, self.height))
+        kernel = np.zeros(5,5)
         
         sum = 0
         for x in range(0, 5):
             for y in range(0, 5):
-                kernel[x,y] = kx[x]*ky[y]
-                sum = kernel[x,y] + sum
+                kernel[x][y] = kx[x]*ky[y]
+                sum = kernel[x][y] + sum
                 
         for x in range(0, 5):
             for y in range(0, 5):
-                kernel[x,y] = kernel[x,y]/sum
-                
+                kernel[x][y] = kernel[x][y]/sum
+        
+        pyrGrad1 = np.zeros(self.width, self.height)
+        pyrGrad2 = np.zeros(self.width, self.height)
+        
+        for x in range (0, self.width):
+            for y in range(0, self.height):
+                pyrGrad1[x][y] = (Xfiltered[x]/2,Yfiltered[y]/2)
+                #NOT ENTIRELY SURE ABOUT THE SECOND PART IF IT'S CORRECT INDEX
+                #should maybe be [0]?
+                pyrGrad2[x][y] = np.sqrt(np.power(pyrGrad1[x][y][0],2)+np.power(pyrGrad1[x][y][1],2))
+                flat[i] = pyrGrad2[x][y]
+                i++
+        
+        fAlpha = 0.1*np.mean(np.reshape(pyrGrad2, self.width, self.height))
+        
         tempImg = logLuminance
         
-        for i in range(0, numPyr - 1):
-                tempImg=np.imresize(convolve(tempImg,kernel),0.5,'bilinear', 'L')
-                Fx = np.correlate(tempImg, Xkernel)/(2^(i+1))
-                Fy = np.correlate(tempImg, Ykernel)/(2^(i+1))
-                pyramidGradient = [pyramidGradient, (Fx/(2^(i+1)),Fy/(2^(i+1)))]
-
-            #Generate attenuation mask
+        for i in range(1,numPyr):
+            tempImg=np.reshape(tempImg, self.width*self.height)
+            resImg = np.convolve(tempImgflat, kernel, "same")
+            tempImg = np.reshape(resImg, self.width, self.height)
+            resImg = np.convolve(tempImg,kernel, "same")
+            tempImg=np.imresize(resImg,50,'bilinear', 'L')
+            Fx = np.correlate(tempImg, Xkernel)/(2^(i+1))
+            Fy = np.correlate(tempImg, Ykernel)/(2^(i+1))
+            pyrGrad1 = [pyrGrad1, (Fx/(2^(i+1)),Fy/(2^(i+1)))]
             
-            
-            
-    def divGback(self):
-        '''Calculating the divergence with backward differences'''
-        G = struct('fx',G(1).fx.*Phi_kp1,'fy',G(1).fy.*Phi_kp1);
-        Xkernel = [[0, 0, 0],
-                   [-1, 0, 1],
-                   [0, 0, 0]]
+        return numPyr, fAlpha
 
-        Ykernel = [[0,1,0],
-                   [0,0,0],
-                   [0,-1,0]]
-        dx = imfilter(G(1).fx.*Phi_kp1,kernelX,'same');
-        dy = imfilter(G(1).fy.*Phi_kp1,kernelY,'same');
-        divG = RemoveSpecials(dx+dy);
-
-   
-    def clampImage(self, Ld):
+    def FattalPhi(self, gradX, gradY, fAlpha):
         
-        for x in range(0, self.width):
-            for y in range(0, self.height):
-                if (Ld[x,y]>1):
-                    Ld[x,y]=1
-                elif (Ld[x,y]<0):
-                    Ld[x,y]=0
+        imgOut = np.zeros(self.width, self.height)
         
-        return Ld
+        for x in range(0, len(fAlpha)):
+            for y in range(0, len(self.fBeta-1)):
+                grad = np.sqrt(np.power(gradX[x],2)+np.power(gradY[y],2))
+        
+        for x in range(0, len(fAlpha)):
+            for y in range(0, len(self.fBeta-1)):
+                t=np.power((grad[x,y]/fAlpha),(self.fBeta-1))
+        
+        for i in range(0, len(t)):
+            if(t=='Inf'):
+                imgOut(i) = 0
+                imgOut(i) = mean(np.reshape(imgOut, self.width*self.height, 1))
+            else:
+                imgOut(i) = t
+
+        return imgOut
+
+    def attenuationMask(self, nP, fAlpha, pg1, pg2):
+        '''Generate attenuation mask'''
+        Phi_kp1 = self.FattalPhi(pg1[nP+1][0], pg1[nP+1][1], fAlpha, self.fBeta)
+
+        for k in range (0,nP):
+            [r,c] = np.size(pg1[nP-k][0])
+            G2 = np.sqrt(np.power(pg1[nP-k][0],2)+np.power(pg1[nP-k][1],2))
+            fAlpha = 0.1*np.mean(np.reshape(G2, np.size(G2), 1))
+            Phi_k = self.FattalPhi(pg1(nP-k).fx, pg1(nP-k).fy, fAlpha, self.fBeta)
+            percentage = int(r/self.width)
+            
+            '''WHY THE HELL IS THAT IMPORT NOT REGOGNIZED!!!!???'''
+            Phi_kp1 = np.imresize(Phi_kp1,50,'bilinear', 'L')
+            for x in range (0, self.width):
+                for y in range(0, self.height):
+                    Phi_kp1[x,y] = Phi_kp1*Phi_k
+
     
     def execute(self):
         
@@ -211,17 +183,22 @@ end'''
             luminanceChannel = self.getLuminanceFromRGB()
             logLuminance = np.log(luminanceChannel+1e-6)
             
-            '''compute Gaussian Pyramid and Gradient'''
-            '''generate the attenuation mask'''
-            
-            '''calculate the divergence with backward differences'''
-            
-            '''solve poisson equation'''
-            Ld = exp(PoissonSolver(divG));
-            '''clamp image'''
-            newLuminance = self.clampImage(Ld)
-            '''change luminance (render the image)'''
+            #compute Gaussian Pyramid and Gradient
+            #generate the attenuation mask
+            #calculate the divergence with backward differences
+            #solve poisson equation
+            divG = 0
+            PoissonSolver = 0
+            Ld = np.exp(PoissonSolver(divG));
+            #clamp image
+            for x in range(0, self.width - 1):
+                for y in range(0, self.height - 1):
+                    if (Ld[x,y]>1):
+                        Ld[x,y]=1
+                    elif (Ld[x,y]<0):
+                        Ld[x,y]=0
+            #change luminance (render the image)
+            newLuminance = 0
             self.modifyLuminance(newLuminance)
             self.saveTemp()
-
     
